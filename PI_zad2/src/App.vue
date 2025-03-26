747b909f473e7551460137e8a34afca6401b1927
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2025-03-26 10:04:26 +0100

    commit

---
+++ PI_zad2/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="
+  text-lime-300
+  bg-zinc-800/90
+  border-sky-950
+  border-2
+  ">
+  Some text
+  </div>
+ </template>
+
+ <style scoped>
+ 
+ </style>
