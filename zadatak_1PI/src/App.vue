9bd495e4640474df460e1925a6d6a547ac859fd3
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2025-04-03 18:18:26 +0200

    Update App.vue

--- zadatak_1PI/src/App.vue
+++ zadatak_1PI/src/App.vue
@@ -12,42 +12,49 @@
         "
     >
 
-        <div class="
+        <h1 class="
             text-6xl
             text-center">
             Pozdrav, ja sam <span class="text-cyan-400">Luka Ritoša</span>
-        </div>
+        </h1>
         <hr class="w-[1024px] border-width-[2px] capacity-[75%] border-radius-[9999px]">
-        <div class="text-[24px]">
+        <h2 class="text-[24px]">
             Moji omiljeni filmovi:
-        </div>
+        </h2>
         
         <div class="flex gap-20 justify-center">
-            <img src="https://d135u4jtzauizi.cloudfront.net/GTM_KEY_0097_1SHEET_F04.jpg"
-            class="
-                w-[320px]
-                h-[480px]
-                border-radius-[6px]" 
-            >
-            
-            <img src="https://resizing.flixster.com/iss8ZwdDwd8Be0ZLtra3TBL37MQ=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzFmMGJiZjdmLTQwZWEtNGRiZi04YzQ0LWE1NmVlOTJhYzE5Yy53ZWJw"
-            class="
-                w-[320px]
-                h-[480px]
-                border-radius-[6px]" 
-            >
-            <img src="https://image.tmdb.org/t/p/original/iH8saz6s0Z8SPPrKaMI2KrRzTED.jpg"
-            class="
-                w-[320px]
-                h-[480px]
-                border-radius-[6px]" 
-            >
+            <div class="text-center">
+                <img src="https://d135u4jtzauizi.cloudfront.net/GTM_KEY_0097_1SHEET_F04.jpg"
+                class="
+                    w-[320px]
+                    h-[480px]
+                    border-radius-[6px]" 
+                >
+                <p class="text-lg md:text-xl text-teal-100 mt-4">Good time</p>
+            </div>
+
+            <div class="text-center">
+                <img src="https://resizing.flixster.com/iss8ZwdDwd8Be0ZLtra3TBL37MQ=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzFmMGJiZjdmLTQwZWEtNGRiZi04YzQ0LWE1NmVlOTJhYzE5Yy53ZWJw"
+                class="
+                    w-[320px]
+                    h-[480px]
+                    border-radius-[6px]" 
+                >
+            <p class="text-lg md:text-xl text-teal-100 mt-4">Tokyo godfathers</p>
+            </div>
+
+            <div class="text-center">
+                <img src="https://image.tmdb.org/t/p/original/iH8saz6s0Z8SPPrKaMI2KrRzTED.jpg"
+                class="
+                    w-[320px]
+                    h-[480px]
+                    border-radius-[6px]" 
+                >
+                <p class="text-lg md:text-xl text-teal-100 mt-4">La haine</p>
+            </div>
         </div>
 
-    </div>
-    <div>
-
-    </div>    
+    </div>   
 </template>
 
 <style scoped>
