c93f71865a0bf18ff6b07e15bb670122d568d6bb
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2025-04-03 19:17:29 +0200

    Update App.vue

--- zadatak_1PI/src/App.vue
+++ zadatak_1PI/src/App.vue
@@ -5,56 +5,77 @@
 <template>
 
     <div class="
+        min-h-screen 
+        flex 
+        items-center 
+        justify-center 
         bg-gradient-to-br 
-        from-emerald-700 
-        to-indigo-700 
-        text-teal-100
-        "
-    >
+        from-red-800 
+        to-slate-800 
+        text-rose-50 "> 
 
-        <h1 class="
-            text-6xl
-            text-center">
-            Pozdrav, ja sam <span class="text-cyan-400">Luka Ritoša</span>
-        </h1>
-        <hr class="w-[1024px] border-width-[2px] capacity-[75%] border-radius-[9999px]">
-        <h2 class="text-[24px]">
-            Moji omiljeni filmovi:
-        </h2>
-        
-        <div class="flex gap-20 justify-center">
-            <div class="text-center">
-                <img src="https://d135u4jtzauizi.cloudfront.net/GTM_KEY_0097_1SHEET_F04.jpg"
-                class="
-                    w-[320px]
-                    h-[480px]
-                    border-radius-[6px]" 
-                >
-                <p class="text-lg md:text-xl text-teal-100 mt-4">Good time</p>
+        <div >
+
+            <h1 class="
+                text-6xl
+                text-center
+                ">
+                Pozdrav, ja sam <span class="text-stone-400">Luka Ritoša</span>
+            </h1>
+            <hr class="
+                mx-auto 
+                my-6
+                border-2
+                rounder-full
+                opacity-75
+                w-[1024px]">
+            <h2 class="
+                text-[24px] 
+                mb-6">
+                Moji omiljeni filmovi:
+            </h2>
+            
+            <div class="    
+                flex  
+                gap-6 
+                justify-center 
+                text-[24px]">
+
+                <div class="text-center">
+                    <img src="https://d135u4jtzauizi.cloudfront.net/GTM_KEY_0097_1SHEET_F04.jpg"
+                    class="
+                        w-[320px]
+                        h-[480px]
+                        border-radius-[6px]" 
+                    >
+                    <p >Good Time</p>
+                </div>
+
+                <div class="text-center">
+                    <img src="https://resizing.flixster.com/iss8ZwdDwd8Be0ZLtra3TBL37MQ=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzFmMGJiZjdmLTQwZWEtNGRiZi04YzQ0LWE1NmVlOTJhYzE5Yy53ZWJw"
+                    class="
+                        w-[320px]
+                        h-[480px]
+                        border-radius-[6px]" 
+                    >
+                <p>Tokyo Godfathers</p>
+                </div>
+
+                <div class="text-center">
+                    <img src="https://image.tmdb.org/t/p/original/iH8saz6s0Z8SPPrKaMI2KrRzTED.jpg"
+                    class="
+                        w-[320px]
+                        h-[480px]
+                        border-radius-[6px]" 
+                    >
+                    <p>La Haine</p>
+                </div>
             </div>
 
-            <div class="text-center">
-                <img src="https://resizing.flixster.com/iss8ZwdDwd8Be0ZLtra3TBL37MQ=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzFmMGJiZjdmLTQwZWEtNGRiZi04YzQ0LWE1NmVlOTJhYzE5Yy53ZWJw"
-                class="
-                    w-[320px]
-                    h-[480px]
-                    border-radius-[6px]" 
-                >
-            <p class="text-lg md:text-xl text-teal-100 mt-4">Tokyo godfathers</p>
-            </div>
+        </div>  
+    
+    </div>    
 
-            <div class="text-center">
-                <img src="https://image.tmdb.org/t/p/original/iH8saz6s0Z8SPPrKaMI2KrRzTED.jpg"
-                class="
-                    w-[320px]
-                    h-[480px]
-                    border-radius-[6px]" 
-                >
-                <p class="text-lg md:text-xl text-teal-100 mt-4">La haine</p>
-            </div>
-        </div>
-
-    </div>   
 </template>
 
 <style scoped>
