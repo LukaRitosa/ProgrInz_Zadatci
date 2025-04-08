9d68d1e4e36184f1a60db150f38185f3b79387ed
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2025-04-08 11:27:08 +0200

    commit

---
+++ PrimjerToDo/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="">
+  ačopefkwefk
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ PrimjerToDo/src/components/ToDoBody.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="">
+  
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
