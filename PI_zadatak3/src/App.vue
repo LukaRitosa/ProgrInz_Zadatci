6cf78b935788aab8eb946a7de2a504f29a36a808
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2025-04-10 19:27:30 +0200

    commit

--- PI_zadatak3/src/App.vue
+++ PI_zadatak3/src/App.vue
@@ -1,35 +1,50 @@
 <script setup>
  import { ref } from "vue";
- const lozinka = ref("")
-
-
+ const newName = ref('')
+ const names = ref([
+ "Ana",
+ "Marko",
+ "Petra",
+ "Ivan"
+ ]);
 </script>
-
 <template>
- 
- 
- <b>Lozinka:</b> <input type="text" v-model="lozinka" class="border p-1 rounded">
-
- <div class="text-red-600">
-  <div v-if="lozinka.length<8">
-    Lozinka pre kratka
-  </div>
-  <div v-if="!/[A-Z]/.test(lozinka)">
-    Lozinka nema veliko slovo
-  </div>
-  <div v-if="!/[!@#$%^&*(),.?:{}|<>]/.test(lozinka)">
-    Lozinka nema poseban znak
-  </div>
-  <div v-if="!/[0-9]/.test(lozinka)">
-    Lozinka nema broj
-  </div>
-</div>
-
- <div v-if="/[A-Z]/.test(lozinka) && /[!@#$%^&*(),.?:{}|<>]/.test(lozinka) && /[0-9]/.test(lozinka) && lozinka.length>=8" 
-  class="text-green-600">
-  Lozinka sigurna
+ <div class="flex flex-wrap gap-2">
+ <input type="text" v-model="newName" class="border px-1 w-32 rounded"
+placeholder="Novo ime...">
+ <button @click="names.push(newName)"
+ class="p-1 bg-blue-200 hover:bg-green-200 cursor-pointer rounded">
+ push()
+ </button>
+ <button @click="names.pop()"
+ class="p-1 bg-blue-200 hover:bg-green-200 cursor-pointer rounded">
+ pop()
+ </button>
+ <button @click="names.unshift(newName)"
+ class="p-1 bg-blue-200 hover:bg-green-200 cursor-pointer rounded">
+ unshift()
+ </button>
+ <button @click="names.shift()"
+ class="p-1 bg-blue-200 hover:bg-green-200 cursor-pointer rounded">
+ shift()
+ </button>
+ <button @click="names.reverse()"
+ class="p-1 bg-blue-200 hover:bg-green-200 cursor-pointer rounded">
+ reverse()
+ </button>
+ <button @click="names.sort()"
+ class="p-1 bg-blue-200 hover:bg-green-200 cursor-pointer rounded">
+ sort()
+ </button>
+ </div>
+ <hr class="my-2">
+ <div class="flex flex-col gap-1 text-sm">
+ <div v-for="(name, index) in names" class="px-1 rounded w-fit">
+ <b>{{index}}.</b> {{ name }}
+ </div>
  </div>
 </template>
+
 
 <style scoped>
 
