d8ca8401aac3c908816db88fca686829cad0c5c8
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2025-04-24 17:04:39 +0200

    commit

---
+++ PI_zadatak4/src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+    import { ref } from 'vue'
+
+    import Header from '@/components/Header.Vue'
+    import Footer from '@/components/Footer.Vue'
+    import Navbar from '@/components/Navbar.Vue'
+    import Početna from '@/components/PagePočetna.Vue'
+    import Slika from '@/components/PageSlika.vue'
+    import Grafika from '@/components/PageGrafika.vue'
+
+    const links = [
+        { name: "POČETNA", active: true},
+        { name: "SLIKA", active: true},
+        { name: "ZVUK", active: false},
+        { name: "GRAFIKA", active: true},
+        { name: "MODEL", active: false},
+        { name: "VIDEO", active: false}
+    ];
+    
+    const activeLink = ref("POČETNA");
+
+    function updateActiveLink(newLink) {
+      activeLink.value = newLink
+    }
+
+</script>
+
+<template>
+    <div class="fixed w-full h-full flex justify-center poppins text-sm text-indigo-300
+        bg-gradient-to-b from-[#1B1A55] via-[#2B428D] to-[#3B39BB]">
+
+        <div style="background-image: url('./src/assets/images/pattern.jpg'); 
+            background-size: 200px;"
+            class="fixed w-full h-full bg-repeat opacity-[3%] -z-10"></div>
+
+        <div class="bg-[#535C91]/25 border-x border-[#9290C3]/10 w-[700px] max-w-[700px] shadow-lg
+            flex flex-col justify-between px-8 gap-4 overflow-auto">
+                
+            <Header/>
+
+
+            <Navbar
+              :links="links"
+              :activeLink="activeLink"
+              @updateActiveLink="updateActiveLink"
+            />
+
+
+            <Početna v-if="activeLink == 'POČETNA'"/>
+
+            <Slika v-if="activeLink == 'SLIKA'"/>
+
+            <Grafika v-if="activeLink == 'GRAFIKA'"/>
+
+            <Footer/>
+        </div>
+    </div>
+
+</template>
---
+++ PI_zadatak4/src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<script setup>
+    import { defineProps, defineEmits } from 'vue'
+
+    const props = defineProps({
+        activeLink: String,
+        links: Array
+    })
+
+    const emit = defineEmits(['updateActiveLink'])
+
+    function setActive(linkName) {
+        emit('updateActiveLink', linkName)
+    }
+</script>
+
+<template>
+  <nav class="sticky top-0 w-full flex gap-4 flex-wrap items-center justify-around bg-[#070F2B]/25 backdrop-blur-sm py-2 px-8 rounded-lg">
+    <div
+      v-for="link in links" :key="link.name" class="text-sm z-10 relative transition" 
+      @click="setActive(link.name)"
+      :class="[link.name == activeLink ? 'underline text-purple-100' : 'cursor-pointer text-indigo-300 text-sm hover:text-indigo-200 hover:scale-105',  link.active ? '' : 'text-indigo-500/50 text-sm pointer-events-none font line-through']">
+      
+      {{ link.name }}
+      
+      <b v-if="link.name == activeLink" class="text-purple-400 -z-[5] absolute -left-0.5 blur-sm">
+        {{ link.name }}
+      </b>
+
+    </div>
+  </nav>
+</template>
+
