37c0b047df3bbd6790923422b4dc9c43e666d850
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2025-05-13 08:51:20 +0200

    commit

--- vjezba_na_satu1/src/main.js
+++ vjezba_na_satu1/src/main.js
@@ -3,4 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import routers from './router'
+
+
+
 createApp(App).mount('#app')
---
+++ vjezba_na_satu1/src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+    <b>
+        Hello
+    </b>
+</template>
